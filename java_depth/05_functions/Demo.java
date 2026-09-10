c9cf0253910a1038611539ecc8dffde9fa181458
Prashant-Kumar <prashantkumar6425@gmail.com>
2026-09-10 11:51:25 +0530

    add functions & its four type

---
+++ java_depth/05_functions/Demo.java
@@ -0,0 +1,34 @@
+public class Demo {
+    public static void main(String[] args) {
+        // Functions in java
+        // 1. No input and no output
+        greet();
+        // 2. Input but no output
+        greet("Prashant");
+        // 3. No input but output
+        System.out.println(getNum());
+        // 4. Input and output
+        System.out.println(getDetails("Prashant", 23));
+    }   
+    // No input and no output
+    static void greet(){
+        System.out.println("Hello!");
+        return;
+    } 
+
+    // input But no output
+    static void greet(String name) {
+        System.out.println("Hello " + name  + "!");
+        return;
+    }
+
+    static String getDetails(String name, int age) {
+        return name + " is " + age + " years old";
+    }
+
+    // no input but output
+    static int getNum() {
+        int c = (int)(Math.random() * 100);
+        return c;
+    }
+}
