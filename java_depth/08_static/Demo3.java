a50070866c9949726f32a1a453466f9edf92734f
Prashant-Kumar <prashantkumar6425@gmail.com>
2026-09-13 18:59:31 +0530

    Add usage example for command-line arguments in Demo3 class

--- java_depth/08_static/Demo3.java
+++ java_depth/08_static/Demo3.java
@@ -7,5 +7,7 @@ public class Demo3 {
         for (int i = 0; i < args.length; i++ ) {
             System.out.println("Argument " + i + " is " + args[i]);
         }
+
+        // java Demo3 arg1 arg2 arg3
     }    
 }
