64e165996b2af7ba0ce689bf7633ae19b3cb2249
Prashant-Kumar <prashantkumar6425@gmail.com>
2026-09-19 13:25:14 +0530

    add: concept of POJO classes

---
+++ java_depth/16_pojo_classes/Demo.java
@@ -0,0 +1,17 @@
+// POJO(Plain Old Java Classes) classes 
+// - No hardcode business logic
+// - Having (getter/setter, constructors, variables)
+// - Builder
+// - business logic 
+
+// Types:
+// 1. Anemic Model (getters/setters, constructors, fields)
+// 2. Rich domain Model (some of bisuness logic have this)
+
+
+public class Demo {
+    public static void main(String[] args) {
+        
+    }    
+}
+
